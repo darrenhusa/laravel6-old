cf0f3c64fd75bd053a60fa6e85b4f5dc9ee766b8
Darren Henderson <darrenh@Darrens-MBP.netgear.com>
2019-09-22 18:38:18 -0500

    Added Post model, migration, and controller. Added route to show post. Create show post view.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function show($post)
+    {
+      $post = 'first-post';
+      // dd($post);
+      // return 'hello';
+      return view('post', [
+        'post' => $post,
+      ]);
+    }
 }
