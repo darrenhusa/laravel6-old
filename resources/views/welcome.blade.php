9afe226a8491273affd97bd79accdb8d3a7d8b74
Darren Henderson <darrenh@Darrens-MBP.netgear.com>
2019-09-22 13:48:29 -0500

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel 6</title>
+
+        
+    </head>
+    <body>
+        
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel 6
+                </div>
+
+            </div>
+        </div>
+    </body>
+</html>
